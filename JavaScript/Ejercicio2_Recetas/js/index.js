2cde67ece1755ae4ebc4e5ccfe94a560ccc5e62d
jackeline <jackito_22062004@hotmail.com>
2018-11-16 16:21:57 -0500

    U2.Lección 1: Introducción a la programación con JavaScript
    U2.Lección 1: Introducción a la programación con JavaScript

---
+++ JavaScript/Ejercicio2_Recetas/js/index.js
@@ -0,0 +1,26 @@
+
+document.getElementById('boton-receta1').addEventListener('click', function(){
+  var ingredientes=["Claras de huevo", "Azucar Blanca", "Leche Condensada",  "Gelatina sin sabor", "Limones"];
+  var cantidades=["5 unidades", "150 gr", "200 gr", "1 sobre", "4 unidades"];
+
+  console.log("Preparando Postre de limón");
+  console.log("Primero debes adicionar las "+cantidades[0]+" de "+ingredientes[0]+" en un Bol y batir con los "+cantidades[1]+" de "+ingredientes[1]+"; luego debes adicionar los "+cantidades[2]+" de "+ingredientes[2]+" y batir bien. Una vez termines con esto, adiciona el zumo de las "+cantidades[4]+" de "+ingredientes[4]+" y el "+cantidades[3]+" de "+ingredientes[3]+" hidratada en agua tibia. Por último deposita la mezcla en moldes individaules y mételo a la nevera. ¡En 30 minutos to postre estará listo!");
+
+
+
+})
+
+document.getElementById('boton-receta2').addEventListener('click', function(){
+  var ingredientes=["Ginebra", "Vermouth Seco", "Aceituna"];
+  var cantidades=["2.5 onzas", "0.5 onzas", "1 unidad"];
+
+  console.log("Preparando Martini");
+  console.log("Para preparar un Martini básico debes verter las "+cantidades[0]+" de "+ingredientes[0]+" en una coctelera y añadir "+cantidades[1]+" de "+ingredientes[1]+". Para servir, pon la mezcla en una copa tipo Martini y añade "+cantidades[2]+" de "+ingredientes[2]+".");
+
+
+
+
+
+
+
+})
