66d548a224fcc8f2d1f22c6276213e850ead6664
jackeline <jackito_22062004@hotmail.com>
2019-02-05 10:44:28 -0500

    U2.Lección 4: Manejo de Eventos con jQuery

--- Frameworks y Librerías JavaScript/Ejercicio4_Jquery/nextUcodBaseEventosjQuery/js/app.js
+++ Frameworks y Librerías JavaScript/Ejercicio4_Jquery/nextUcodBaseEventosjQuery/js/app.js
@@ -41,5 +41,67 @@ $(document).ready(function(){
         }
 
      });
-    
+
+     $("#psw").select(function(){
+        alert("No puedes copiar la contraseña debes repetirla!");
+        $(this).attr("oncopy","return false");
+        $(this).contextmenu(function(event){
+            event.preventDefault();
+        });
+     });
+
+     $("#psw").keypress(function(event){
+        var key = event.which;
+        if ((key>32&&key<48)||(key>57&&key<65)||(key>90&&key<97)||(key>122&&key<127)||(key>160&&key<256)) {
+            $(this).siblings(".info").css("display","none");
+            $(this).siblings(".errPsw").css("display","block");
+            event.preventDefault();
+        }else {
+            $(this).siblings(".errPsw").css("display","none");
+        }
+     });
+
+     $("#pswRepeat").keyup(function(){
+        var psw= $("#psw").val();
+        var pswRepeat = $(this).val();
+        if(psw != pswRepeat){
+            $(".errPswRepeat").css("display", "block");
+        }else{
+            $(".errPswRepeat").css("display", "none");
+        }
+     });
+
+     $(".imagen").mousedown(function(event){
+        var self = $(this);
+        $(this).addClass("imagen-seleccionada");
+        $(this).on('dragstart', function(event) {
+            event.preventDefault();
+          });
+        $(".prueba-container").mousemove(function(event){
+            var x = event.pageX;
+            var y = event.pageY;
+            self.css("left",(x- 234)+"px");
+            self.css("top",(y- 591)+"px");
+        });     
+     });
+
+     $(".prueba-container").mouseup(function(event){
+        $(event.target).removeClass("imagen-seleccionada");
+        $(this).off("mousemove");
+        var x = parseFloat($(event.target).css("left"));
+        var y = parseFloat($(event.target).css("top"));
+
+        console.log(x+" "+y);
+
+        var id= $(event.target).attr("id");
+        console.log(id);
+        if(id=="p2" && (x>=929 && x <= 1009) && (y>=21 && y<= 101)){
+            $(event.target).css("left","924px");
+            $(event.target).css("top","65px");
+            $(event.target).addClass("imagen-correcta");
+            $(event.target).off("mousedown");
+            console.log("actualizado");
+        }
+     });
+
 });
