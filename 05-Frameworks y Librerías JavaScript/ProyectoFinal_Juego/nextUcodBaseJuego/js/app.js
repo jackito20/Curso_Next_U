e6e1c3070c0aca631bdcd226eca522fcdd8f4beb
jackeline <jackito_22062004@hotmail.com>
2019-02-25 17:22:31 -0500

    .

--- 05-Frameworks y Librerías JavaScript/ProyectoFinal_Juego/nextUcodBaseJuego/js/app.js
+++ 05-Frameworks y Librerías JavaScript/ProyectoFinal_Juego/nextUcodBaseJuego/js/app.js
@@ -46,104 +46,90 @@ function agregarAleatorio(columna){
 
 function validarIguales(){
   var columnas=$(".panel-tablero div");
-  //console.log(columnas);
+  var elementosEliminar = [];
   for(var i=0; i<columnas.length; i++){
-    //console.log("--------columna");
-    //console.log(columnas[i]);
-    //console.log("********imagenes");
     var elementos = $(columnas[i]).find("img");
-    //console.log(elementos);
-    for(var j=elementos.length-1; j>=0; j--){
-      //console.log(elementos[j]);
-      validarArriba(i, j);
 
+    for(var j=elementos.length-1; j>=0; j--){
+      var validados = validarArriba(i, j);
+      if(validados){
+        console.log("VALIDADOS EN COLUMNA "+i);
+        j=j-2;
+      }
     }
+    
   }
 }
 
 function validarArriba(i, j){
-  //console.log(" i "+i+" j "+j);
+  
   columna=$(".panel-tablero div")[i];
   var elementos = [$(columna).find("img")[j], $(columna).find("img")[j-1], $(columna).find("img")[j-2]]
-  /*elemento1=$(columnas).find("img")[j];
-  elemento2=$(columnas).find("img")[j-1];
-  elemento3=$(columnas).find("img")[j-2];*/
-  //console.log(columnas);
+  
   if(elementos[0] && elementos[1] && elementos[2]){
-    /*console.log($(elementos[0]).attr("src"));
-    console.log($(elementos[1]).attr("src"));
-    console.log($(elementos[2]).attr("src"));*/
+    
     if($(elementos[0]).attr("src") == $(elementos[1]).attr("src")
       && $(elementos[0]).attr("src") == $(elementos[2]).attr("src")){
-        //console.log("DESAPARECER");
-        desaparecer(elementos);
+        console.log("DESAPARECER");
+        console.log("DESAPARECER ELEMENTOS columna ");
+        console.log($(elementos[0]).parent())
+        console.log(elementos);
+        parpadear(elementos, 7);
+        return true;
       }
   }else{
     /*console.log(elementos[0]);
     console.log("NO HAY HACIA ARRIBA");*/
   }
+  return false;
 }
 
-function desaparecer(elementos){
-  //for(i=0; i<3; i++){
-    /*$(elementos[i]).hide(2000, function(i){
-      $(this).remove();
-      agregarQuitados();
-    });*/
-    console.log("DESAPARECER ELEMENTOS ");
-    console.log(elementos);
-    for(i=0; i<3; i++){
-      $(elementos[i]).fadeToggle({
+
+function parpadear(elemento, t){
+  t--;
+  var ap=0;
+  var agregados = [];
+  $("#score-text").text(t);
+  if(t>0){
+    $(elemento).fadeToggle(300, function(){
+      parpadear($(this), t);
+    })
+  }else{
+    $(elemento)
+      .hide({
         duration: 3000,
-        complete: function(){
-          console.log("ELIMINADO");
-          console.log($(this));
+        start: function(){
           var padre = $(this).parent()
-          /*$(this).remove();
-          agregarQuitados(padre);*/
+          
+          agregados.push(agregarQuitados(padre))
+          console.log("AGREGAR ELEMENTOS");
+          console.log(agregados)
+          ap=0
+        },
+        step: function(){
+          for(var i=0; i<agregados.length; i++){
+            $(agregados[i]).show(3000, function(){
+              agregados.shift();
+            });
+          }
+        },
+        complete: function(){
+          $(this).remove().delay(1000);
         }
       });
-    }
-  //}
+  }
 }
 
 function agregarQuitados(columna){
-  //columnas=$(".panel-tablero div");
-  console.log("AGREGAR QUITADOS");
-  console.log(columna);
+  
   elemento=$(columna).find("img")[0];
-  console.log(elemento);
+  
   var img = Math.floor(Math.random()*(4-1+1))+1;
-  $("<img src='image/"+img+".png' class='elemento' style='display:none'>").insertBefore(elemento);
-
-  elementos=$(columna).find("img");
-
-  $(elementos).show({
-    duration: 2000
-  });
-
-  //agregarAleatorio(columna);
-  /*var img = Math.floor(Math.random()*(4-1+1))+1;
-  columna.append("<img src='image/"+img+".png' class='elemento'>");*/
-  /*for(var i=0; i<columnas.length; i++){
-    console.log(columnas[i]);
-    elementos = $(columnas[i]).find("img");
-    console.log(elementos);
-    console.log(elementos.length);
-    if(elementos.length<7){
-      var cant = 7-elementos.length;
-      console.log("AGREGAR "+cant);
-      agregarAleatorio(columnas[i]);
-      /*for(var j=0; j<cant; j++){
-        agregarAleatorio(columnas[i]);
-      }*/
-    /*}
-  }*/
-
-  /*for(i=0; i<cant; i++){
-    console.log("/////Agregar");
-    console.log(columna);
-    agregarAleatorio(columna);
-    alert("agrego");
-  }*/
+  //var elementoAg = $("<img src='image/"+img+".png' class='elemento' style='display:none'>").insertBefore(elemento).hide().fadeIn('slow');
+  
+  var elementoAg = $("<img src='image/"+img+".png' class='elemento' style='display:none'>").insertBefore(elemento);
+  
+  //elementos=$(columna).find("img[style='display:none']");
+  return elementoAg;
+  //$(elementoAg).slideDown(1000);
 }
