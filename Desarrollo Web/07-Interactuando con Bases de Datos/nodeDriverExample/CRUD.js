7595a7749854bdd55aeb7c9011dfb81459c2450f
jackeline <jackito_22062004@hotmail.com>
2019-04-09 21:54:42 -0500

    mongodb

---
+++ Desarrollo Web/07-Interactuando con Bases de Datos/nodeDriverExample/CRUD.js
@@ -0,0 +1,11 @@
+module.exports.insertarRegistro = function(db, callback){
+    let coleccion = db.collection("users")
+
+    await db.collection("users").insertMany([
+        {nombre: "David", edad: 25, peso:75},
+        {nombre: "Steven", edad: 35, peso:80},
+        {nombre: "Fernando", edad: 40, peso:68}
+    ], (error, result) => {
+        console.log("Resultado de insert: "+ result.toString())
+    })
+}
